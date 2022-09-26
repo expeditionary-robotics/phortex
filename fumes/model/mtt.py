cacbca1fb19aa17024061b2ad5b4d116f29b48d2
vpreston <victorialynnpreston@gmail.com>
2022-09-26 01:19:07 -0400

    adds second experimental likelihood function

--- fumes/model/mtt.py
+++ fumes/model/mtt.py
@@ -1043,6 +1043,14 @@ class Crossflow(MTT):
                 return 0.7  # changed on cruise
             else:
                 return 0.0
+        
+        def _likelihood_br_2(xm, x0):
+            if xm == 1:
+                return 0.9
+            elif xm == 0:
+                return 0.3
+            else:
+                return 0.0
 
         def _likelihood(xm, xo):
             if xm == 1 and xo == 1:
@@ -1068,7 +1076,7 @@ class Crossflow(MTT):
             P = mod.get_value(tt, loc[i][:])
             detect = np.log(np.ones_like(P)+P) > thresh
             # errt = [-np.log(_likelihood(d, o)) for d, o in zip(detect, obs[i][:])]
-            err_brier = [(_likelihood_br(d, o) - o)**2 for d, o in zip(detect, obs[i][:])]
+            err_brier = [(_likelihood_br_2(d, o) - o)**2 for d, o in zip(detect, obs[i][:])]
             err_br = err_br + -np.log(np.nanmean(err_brier))  # testing now
             # errt_err = np.nansum(((obs[i][:] - detect)/0.3)**2)
             # err = err + np.nansum(errt)  # this one is kinda bad
